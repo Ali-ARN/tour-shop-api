fd41b0a54c20faa9990d3f6d4e8963500c396a4d
Ali <ali2005afshar@gmail.com>
2023-01-26 16:44:41 +0330

    improved the tour schema

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,48 +1,99 @@
 const mongoose = require("mongoose");
 
-const tourSchema = new mongoose.Schema({
-  // required fields
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    maxLength: [40, "A tour name must be below 40 words"],
-    minLength: [10, "A tour name must be above 10 words"],
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    // required fields
+    name: {
+      type: String,
+      unique: [true, "A tour name must be unique"],
+      required: [true, "A tour must have a name"],
+      maxLength: [40, "A tour name must be below 40 words"],
+      minLength: [10, "A tour name must be above 10 words"],
+    },
 
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-    min: [100, "tour price must be above 100"],
-  },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+      min: [100, "tour price must be above 100"],
+    },
 
-  duration: {
-    type: Number,
-    required: [true, "A tour must have a duration"],
-  },
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+    },
 
-  maxGroupSize: {
-    type: Number,
-    required: [true, "A tour must have a group max number"],
-  },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group max number"],
+    },
 
-  ratingsAverage: {
-    type: Number,
-    required: [true, "A tour must have a ratings average"],
-    min: [1, "tour ratings average must be above 1"],
-    max: [5, "tour ratings average must be below 5"],
-  },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "tour ratings average must be above 1"],
+      max: [5, "tour ratings average must be below 5"],
+    },
 
-  summary: {
-    type: String,
-    required: [true, "A tour must have a summary"],
-  },
+    summary: {
+      type: String,
+      required: [true, "A tour must have a summary"],
+    },
 
-  imageCover: {
-    type: String,
-    required: [true, "A tour must have a image cover"],
-  },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a image cover"],
+    },
 
-  // optional fields
+    // optional fields
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: "price discount ({VALUE}) must be below the actual price",
+      },
+    },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
+    difficulty: {
+      type: String,
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "difficulty must be 'easy', 'medium' or 'difficult'",
+      },
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    images: [String],
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+
+    startDates: [Date],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+tourSchema.virtual("durationWeek").get(function () {
+  return this.duration / 7;
 });
 
 module.exports = mongoose.model("Tour", tourSchema, "tours");
