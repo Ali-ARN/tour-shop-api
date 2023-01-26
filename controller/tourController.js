c1c3ea12264b5738e581ad3fb1a117e3383cba80
Ali <ali2005afshar@gmail.com>
2023-01-26 16:54:47 +0330

    added creating tour feature

--- controller/tourController.js
+++ controller/tourController.js
@@ -34,6 +34,8 @@ exports.getOneTour = catchAsync(async (req, res) => {
 });
 
 exports.createTour = catchAsync(async (req, res) => {
+  const newTour = await Tour.create(req.body);
+
   res.status(200).json({
     status: "success",
     data: {
