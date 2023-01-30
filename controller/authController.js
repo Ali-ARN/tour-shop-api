151ed571db11c98b8a260a1f2e0b13aa5bb9c6e3
Ali <aliafshardev@gmail.com>
2023-01-30 16:37:42 +0330

    Fixed BUG

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,20 @@
 const User = require("../model/userModel");
 const catchAsync = require("../utilities/catchAsync");
+const handleError = require("./errorController");
+const AppError = require("../utilities/appError");
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
 
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+    if (!newUser) return new AppError(404, "something went very wrong");
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      user: newUser,
-    },
-  });
-});
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (error) {
+    handleError(error, req, res);
+  }
+};
