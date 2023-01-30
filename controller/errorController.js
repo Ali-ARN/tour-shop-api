e3e4735f9fd5f83578b3221120a32251c129e673
Ali <aliafshardev@gmail.com>
2023-01-30 16:34:11 +0330

    fixed error handling

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,45 +1,10 @@
-const AppError = require("../utilities/appError");
-
-const handleCastErrorDB = (err) => {
-  const message = `invalid ${err.path}: ${err.value}`;
-  return new AppError(message, 400);
-};
-
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-const sendErrorProd = (err, res) => {
-  // Operational => trusted error => sends error message to the client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  } else {
-    // unknown errors
-    console.error("ERROR: ", err);
-
-    res.status(500).json({
-      status: "error",
-      message: "something went wrong.",
-    });
-  }
-};
 // GLOBAL ERROR HANDLER
 module.exports = (err, req, res) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+  err.statusCode ||= 500;
+  err.status ||= "error";
 
-  if (process.env.NODE_ENV === "developement") {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
-    if (err.name === "CastError") error = handleCastErrorDB(err);
-    sendErrorProd(error, res);
-  }
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 };
