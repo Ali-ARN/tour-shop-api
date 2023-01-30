2e0e9e17188e3b8cb23b509a39b3fc8b94a1e3e6
Ali <aliafshardev@gmail.com>
2023-01-30 16:36:51 +0330

    added hashing the user password feature

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,7 +10,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "A user must have a email"],
-    unique: true,
+    unique: [true, "user email must be unique"],
     validate: [validator.isEmail, "please enter a valid email"],
   },
 
@@ -25,7 +26,26 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "user must confirm his password"],
     minLength: 8,
+    validate: {
+      // only works with save() and create()
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: "passwords are not the same",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only runs this function if password was modified
+  if (!this.isModified("password")) return next();
+
+  // hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete the password confirm
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
