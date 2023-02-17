8d3417516875f97c16376cd2c7505b5306803b4c
Ali <aliafshardev@gmail.com>
2023-02-17 10:28:53 +0330

    Added request limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
 dotenv.config();
 const app = express();
 const tourRoute = require("./routes/tourRoute");
@@ -16,6 +17,14 @@ mongoose
   .then(() => console.log("database connected"))
   .catch((err) => console.error(err.message));
 
+// GLOBAL MIDDLEWARES
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP",
+});
+
+app.use("/api", limiter);
 // ROUTES
 app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
