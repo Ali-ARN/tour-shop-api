3dd8dcfe1fafee001ddfe5ce0964fa02cb8ab742
afshardev <aliafshardev@gmail.com>
2023-03-07 11:22:26 +0330

    Added locations modelling

--- model/tourModel.js
+++ model/tourModel.js
@@ -82,6 +82,31 @@ const tourSchema = new mongoose.Schema(
     },
 
     startDates: [Date],
+
+    startLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"],
+        },
+
+        coordinates: [Number],
+        address: String,
+        description: String,
+      },
+    ],
   },
   {
     toJSON: {
